f350e2684f628aa293b11b37eff33944a1cfe51a
bu <bu@localhost.localdomain>
2015-04-26 12:43:19 +0300

    Add styles to Post form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_comment, only: [:destroy]
+  before_filter :authenticate_user!, :only => [:create]
 
   def create
-    @post = Post.find_by(params[:posy_id])
+    @post = Post.find(params[:post_id])
     comment = @post.comments.build(comment_params)
+    comment.user = current_user
 
     if comment.save
       redirect_to post_path(@post)
@@ -19,13 +21,11 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:content, :post_id, :user_id)
+      params.require(:comment).permit(:content, :post_id)
     end
 end
