ad02ac653d88a5a5d461f124382b067525fa26bb
bu <bulat87@gmail.com>
2015-06-08 22:55:37 +0300

    add IP geodata

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# blog
+Test blog app.
+
+You can register and add your posts.
+App have text editor and geodata.
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def geo_info
+    ip_meta = Ipgeobase.lookup(request.remote_ip)
+    ip_meta.city
+    request.remote_ip
+  end
+
+  def user_owner(item)
+  	current_user == item.user
+  end
+
 end
