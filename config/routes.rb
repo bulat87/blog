e8361051585f9624f7d97fd7bd31ffd44aae7bff
bu <bulat87@gmail.com>
2015-07-07 23:31:18 +0300

    Add web namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
-  resources :posts do
-    resources :comments
-  end
+  
   devise_for :users, :controllers => { :registrations => "users/registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'posts#index'
+  
+  scope module: :web do
 
+    root 'posts#index'
+
+    resources :posts do
+      resources :comments
+    end
+  
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
