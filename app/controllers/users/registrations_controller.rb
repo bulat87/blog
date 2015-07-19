b5acc178142d3230560dc36e9893cbb49f4386b1
bu <bulat87@gmail.com>
2015-07-19 22:16:36 +0300

    Add avatar to User

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -61,7 +61,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :name, :lastname, :patronymic, :birthday, :gender, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :name, :lastname, :patronymic, :birthday, :gender, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :avatar, :name, :lastname, :patronymic, :birthday, :gender, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :avatar, :name, :lastname, :patronymic, :birthday, :gender, :password, :password_confirmation, :current_password) }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715191843) do
+ActiveRecord::Schema.define(version: 20150719180034) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -88,6 +88,10 @@ ActiveRecord::Schema.define(version: 20150715191843) do
     t.string   "patronymic"
     t.date     "birthday"
     t.string   "gender"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
